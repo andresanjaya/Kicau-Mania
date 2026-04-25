023dc30962c21da33b3a4b77e2b8d683c91e6037
Andre Sanjaya <test@example.com>
2026-04-25 10:29:58 +0800

    Add Vercel Json

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VERCEL ? '/' : '/Kicau-Mania/',
+  base: process.env.GITHUB_ACTIONS ? '/Kicau-Mania/' : '/',
   build: {
     rollupOptions: {
       output: {
